e0c6aafa27e4cb64a0ba1a8585dd344b47a9a1d4
purofle <purofle@gmail.com>
2022-07-02 18:49:41 +0800

    format and remove styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,42 @@
 import {Button, Typography, Grid, TextField} from '@mui/material'
 import {Container} from '@mui/system'
-import { NextPage } from 'next'
+import {NextPage} from 'next'
 import Head from 'next/head'
-import {useCallback, useEffect, useState} from 'react'
+import {useState} from 'react'
 import checkScore from './api/echeck-score'
 
 const Home: NextPage = () => {
-  const [exam, setExam] = useState(0)
-  const [idCard, setidCard] = useState('')
-  const [score, setScore] = useState('')
+	const [exam, setExam] = useState(0)
+	const [idCard, setidCard] = useState('')
+	const [score, setScore] = useState('')
 
-  async function getScore() {
-	const result = await checkScore(exam, idCard)
-	setScore(result)
-	console.log(result)
+	async function getScore() {
+		const result = await checkScore(exam, idCard)
+		setScore(result)
+		console.log(result)
 	}
 
-  return (
-  <>
-  <Head>
-  <meta name="viewport" content="initial-scale=1, width=device-width" />
-  </Head>
-  <Container>
-  <Grid container spacing={1} direction="column" justifyContent="center" alignItems="center">
-  <Grid item xs="auto">
-  <TextField id="outlined-basic" label="准考证号" variant="outlined" onChange={event => { setExam(parseInt(event.target.value)) }}/>
-  </Grid>
-  <Grid item xs="auto">
-  <TextField id="outlined-basic" label="身份证后6位" variant="outlined" onChange={ event => { setidCard(event.target.value) } }/>
-  </Grid>
-  <Grid item xs="auto">
-  <Button variant="contained" onClick={async() => {await getScore()}}>查询</Button>
-  </Grid>
-  <Typography>{score.toString()}</Typography>
-  </Grid>
-  </Container>
-  </>
-)}
+	return (
+		<>
+			<Head>
+				<meta name="viewport" content="initial-scale=1, width=device-width" />
+			</Head>
+			<Container>
+				<Grid container spacing={1} direction="column" justifyContent="center" alignItems="center">
+					<Grid item xs="auto">
+						<TextField id="outlined-basic" label="准考证号" variant="outlined" onChange={event => {setExam(parseInt(event.target.value))}} />
+					</Grid>
+					<Grid item xs="auto">
+						<TextField id="outlined-basic" label="身份证后6位" variant="outlined" onChange={event => {setidCard(event.target.value)}} />
+					</Grid>
+					<Grid item xs="auto">
+						<Button variant="contained" onClick={async () => {await getScore()}}>查询</Button>
+					</Grid>
+					<Typography>{score.toString()}</Typography>
+				</Grid>
+			</Container>
+		</>
+	)
+}
 
 export default Home
